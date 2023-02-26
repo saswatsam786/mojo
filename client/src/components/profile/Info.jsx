73ca0c74847372ee76ea9349ea1f2e9e68773fcd
saswatsam786 <saswat.sam786@gmail.com>
2023-02-26 07:12:49 +0530

    simple change

--- client/src/components/profile/Info.jsx
+++ client/src/components/profile/Info.jsx
@@ -43,68 +43,74 @@ const Info = () => {
   let check = true;
   return (
     <div className="info">
-      {userData.map((user) => {
-        if (check) {
-          check = false;
-          return (
-            <div className="info_container" key={user._id}>
-              <Avatar src={user.avatar} size="supper-avatar" />
-              <div className="info_content">
-                <div className="info_content_title">
-                  <h2>{user.username}</h2>
-                  {user._id === auth.user._id ? (
-                    <button
-                      className="btn btn-outline-info"
-                      onClick={() => setOnEdit(true)}
+      {
+        // eslint-disable-next-line
+        userData.map((user) => {
+          if (check) {
+            check = false;
+            return (
+              <div className="info_container" key={user._id}>
+                <Avatar src={user.avatar} size="supper-avatar" />
+                <div className="info_content">
+                  <div className="info_content_title">
+                    <h2>{user.username}</h2>
+                    {user._id === auth.user._id ? (
+                      <button
+                        className="btn btn-outline-info"
+                        onClick={() => setOnEdit(true)}
+                      >
+                        Edit Profile
+                      </button>
+                    ) : (
+                      <FollowBtn user={user} />
+                    )}
+                  </div>
+
+                  <div className="follow_btn">
+                    <span
+                      className="mr-4"
+                      onClick={() => setShowFollowers(true)}
                     >
-                      Edit Profile
-                    </button>
-                  ) : (
-                    <FollowBtn user={user} />
-                  )}
+                      {user.followers.length} Followers
+                    </span>
+                    <span
+                      className="ml-4"
+                      onClick={() => setShowFollowings(true)}
+                    >
+                      {user.following.length} Following
+                    </span>
+                  </div>
+                  <h6>
+                    {user.fullname}{" "}
+                    <span className="text-danger">{user.mobile}</span>
+                  </h6>
+                  <p className="m-0">{user.address}</p>
+                  <h6 className="m-0">{user.email}</h6>
+                  <a href={user.website} target="_blank" rel="noreferrer">
+                    {user.website}
+                  </a>
+                  <p>{user.story}</p>
                 </div>
+                {onEdit && <EditProfile setOnEdit={setOnEdit} />}
 
-                <div className="follow_btn">
-                  <span className="mr-4" onClick={() => setShowFollowers(true)}>
-                    {user.followers.length} Followers
-                  </span>
-                  <span
-                    className="ml-4"
-                    onClick={() => setShowFollowings(true)}
-                  >
-                    {user.following.length} Following
-                  </span>
-                </div>
-                <h6>
-                  {user.fullname}{" "}
-                  <span className="text-danger">{user.mobile}</span>
-                </h6>
-                <p className="m-0">{user.address}</p>
-                <h6 className="m-0">{user.email}</h6>
-                <a href={user.website} target="_blank" rel="noreferrer">
-                  {user.website}
-                </a>
-                <p>{user.story}</p>
+                {showFollowers && (
+                  <Followers
+                    users={user.followers}
+                    setShowFollowers={setShowFollowers}
+                  />
+                )}
+
+                {showFollowings && (
+                  <Followings
+                    users={user.following}
+                    setShowFollowings={setShowFollowings}
+                  />
+                )}
               </div>
-              {onEdit && <EditProfile setOnEdit={setOnEdit} />}
-
-              {showFollowers && (
-                <Followers
-                  users={user.followers}
-                  setShowFollowers={setShowFollowers}
-                />
-              )}
-
-              {showFollowings && (
-                <Followings
-                  users={user.following}
-                  setShowFollowings={setShowFollowings}
-                />
-              )}
-            </div>
-          );
-        }
-      })}
+            );
+          }
+        })
+      }
     </div>
   );
 };
